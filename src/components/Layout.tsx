ac69663e45c5f94aeb538dc138769dc3cdd2c1c4
Konstantin Simeonov <kon.simeonov@protonmail.com>
2023-02-06 19:18:05 +0200

    fix: fix that layout a bit

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,8 +6,8 @@ export const Layout: React.FC<React.PropsWithChildren<{}>> = ({ children }) => (
   <div className="stack">
     <Nav />
     <div className="cluster">
-      <main className="box">{children}</main>
-      <aside><MostRecent /></aside>
+      <main className="box" style={{ flex: 1 }}>{children}</main>
+      <aside style={{ width: `20%` }}><MostRecent /></aside>
     </div>
   </div>
 )
