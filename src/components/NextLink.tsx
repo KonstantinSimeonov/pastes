1f8a7933b87a8d52c122ca1f1804dfd1c5b4857e
Konstantin Simeonov <kon.simeonov@protonmail.com>
2023-02-16 12:01:19 +0200

    fix(NextLink): fix hydration error

--- src/components/NextLink.tsx
+++ src/components/NextLink.tsx
@@ -1,19 +1,9 @@
 import React, { forwardRef, Ref } from "react"
-import Link, { LinkProps } from "next/link"
+import Link from "next/link"
 import { Link as MuiLink, LinkProps as MuiLinkProps } from "@mui/material"
 
-type NextLinkProps = Omit<MuiLinkProps, "href" | "classes"> &
-  Pick<LinkProps, "href" | "as" | "prefetch">
-
-export const NextLink = forwardRef<HTMLAnchorElement, NextLinkProps>(
-  function NextLink(
-    { href, as, prefetch, ...props }: LinkProps,
-    ref: Ref<HTMLAnchorElement>
-  ) {
-    return (
-      <Link href={href} as={as} prefetch={prefetch} passHref>
-        <MuiLink ref={ref} {...props} />
-      </Link>
-    )
+export const NextLink = forwardRef<HTMLAnchorElement, MuiLinkProps>(
+  function NextLink(props: MuiLinkProps, ref: Ref<HTMLAnchorElement>) {
+    return <MuiLink component={Link} ref={ref} {...props} />
   }
 )
