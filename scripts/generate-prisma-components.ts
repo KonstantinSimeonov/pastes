083bd57872916a6f4317829f8b593a4b74d291be
Konstantin Simeonov <kon.simeonov@protonmail.com>
2023-02-16 17:40:11 +0200

    fix(scripts): format script

--- scripts/generate-prisma-components.ts
+++ scripts/generate-prisma-components.ts
@@ -5,9 +5,12 @@ import * as path from "path"
 
 type LangName = keyof typeof components.languages
 
-type Langs = Record<LangName, {
-  require?: LangName | readonly LangName[]
-}>
+type Langs = Record<
+  LangName,
+  {
+    require?: LangName | readonly LangName[]
+  }
+>
 
 const get_deps = (lang: LangName, langs: Langs): LangName[] => {
   const { require } = langs[lang]
@@ -29,24 +32,28 @@ const build_graph = (langs: Langs) => {
 
 const graph_to_imports = (graph: ReturnType<typeof build_graph>) =>
   graph.map(
-    ([k, deps]) => [k, Array.from(new Set(deps), dep => `import "prismjs/components/prism-${dep}"`).join(`\n`)] as const
+    ([k, deps]) =>
+      [
+        k,
+        Array.from(
+          new Set(deps),
+          dep => `import "prismjs/components/prism-${dep}"`
+        ).join(`\n`),
+      ] as const
   )
 
 const write_modules = (imports: ReturnType<typeof graph_to_imports>) => {
   console.log(imports)
   for (const [filename, content] of imports) {
-    const filepath = path.join(process.cwd(), `src`, `prism-components`, `${filename}.ts`)
-    console.log(
-      `Writing to ${filepath}\n`,
-      content,
-      `\n`
+    const filepath = path.join(
+      process.cwd(),
+      `src`,
+      `prism-components`,
+      `${filename}.ts`
     )
+    console.log(`Writing to ${filepath}\n`, content, `\n`)
     fs.writeFileSync(filepath, content, { encoding: `utf8` })
   }
 }
 
-write_modules(
-  graph_to_imports(
-    build_graph(components.languages as Langs)
-  )
-)
+write_modules(graph_to_imports(build_graph(components.languages as Langs)))
