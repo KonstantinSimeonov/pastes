3860d9679d516e0284c6cd71b4b47b9bdc38b7e9
Konstantin Simeonov <kon.simeonov@protonmail.com>
2023-02-18 01:11:52 +0200

    improve(/create): add toasts for success/error

--- src/pages/create.page.tsx
+++ src/pages/create.page.tsx
@@ -2,22 +2,44 @@ import Head from "next/head"
 import { useRouter } from "next/router"
 import * as React from "react"
 import { useMutation } from "@tanstack/react-query"
-import axios from "axios"
+import axios, { AxiosError } from "axios"
 import { post, PostResp } from "@/pages/api/pastes/schemas"
 
 import { PasteForm } from "@/components/PasteForm"
 import { z } from "zod"
+import { useToast } from "@/components/Snackbar"
 
 type Create = z.infer<typeof post>
 
 export default function Create() {
-  const r = useRouter()
+  const router = useRouter()
+  const toast = useToast()
 
   const createPaste = useMutation(
     (p: Create) => axios.post<PostResp>(`/api/pastes`, p),
     {
-      onSuccess: ({ data }) => r.push(`/pastes/${data.id}`),
+      onSuccess: ({ data }) => {
+        toast({
+          severity: `success`,
+          children: `Paste created, redirecting you in a sec...`,
+        })
+        router.push(`/pastes/${data.id}`)
+      },
     }
+  )
+
+  const onSubmit = React.useCallback(
+    (paste: Create) => {
+      toast({ severity: `info`, children: `Creating paste...` })
+      return createPaste.mutateAsync(paste).catch((error: AxiosError) => {
+        console.error(error)
+        toast({
+          severity: `error`,
+          children: `Something went wrong, please retry`,
+        })
+      })
+    },
+    [createPaste.mutateAsync]
   )
 
   return (
@@ -25,10 +47,7 @@ export default function Create() {
       <Head>
         <title>New Paste</title>
       </Head>
-      <PasteForm
-        schema={post}
-        onSubmit={paste => createPaste.mutateAsync(paste)}
-      />
+      <PasteForm schema={post} onSubmit={onSubmit} />
     </>
   )
 }
