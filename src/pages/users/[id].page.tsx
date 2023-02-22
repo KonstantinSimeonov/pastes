26e39efaf7ba3e3ead282fff225b93d6fd283a1c
Konstantin Simeonov <kon.simeonov@protonmail.com>
2023-02-22 13:43:18 +0200

    feat(list-user-pastes): list pastes on users page
    right now this also includes private pastes

--- src/pages/users/[id].page.tsx
+++ src/pages/users/[id].page.tsx
@@ -3,11 +3,11 @@ import React from "react"
 import "node_modules/prismjs/themes/prism-tomorrow.css"
 import Head from "next/head"
 import { withClient } from "@/prisma/with-client"
-import { MostRecent } from "@/components/MostRecent"
 import LC from "language-colors"
 import { EXT_MAP } from "../pastes/extension-map"
-import { Tooltip, Typography } from "@mui/material"
+import { Box, Tooltip, Typography } from "@mui/material"
 import { Stack } from "@mui/system"
+import { NextLink } from "@/components/NextLink"
 
 export const getServerSideProps = async (ctx: GetStaticPropsContext) => {
   const user = await withClient(client =>
@@ -17,6 +17,23 @@ export const getServerSideProps = async (ctx: GetStaticPropsContext) => {
       },
       include: {
         stats: true,
+        pastes: {
+          select: {
+            id: true,
+            description: true,
+            files: {
+              take: 1,
+              select: {
+                name: true,
+                content: true,
+              },
+            },
+          },
+          orderBy: {
+            createdAt: `desc`,
+          },
+          take: 10,
+        },
       },
     })
   )
@@ -57,35 +74,56 @@ export default function UserById({ user, colors }: Props) {
       <Head>
         <title>{user.name}</title>
       </Head>
-      <Stack gap={2}>
-        <Typography variant="h4" component="h1">
-          {user.name} ({user.stats?.totalPastesCount} pastes)
-        </Typography>
-        <Stack direction="row" gap={0} sx={{ width: `20rem`, height: `1rem` }}>
-          {colors.map(({ color, count, name }) => (
-            <Tooltip
-              title={
-                <Typography>
-                  {name} {((count / total) * 100) | 0}%
-                </Typography>
-              }
-              key={`${color?.join()}-${name}`}
-            >
-              <div
-                style={{
-                  width: `${(count / total) * 100}%`,
-                  backgroundColor: `rgb(${color?.join()})`,
-                }}
-              />
-            </Tooltip>
-          ))}
+      <Stack gap={4}>
+        <Stack gap={1}>
+          <Typography variant="h4" component="h1">
+            {user.name} ({user.stats?.totalPastesCount} pastes)
+          </Typography>
+          <Stack
+            direction="row"
+            gap={0}
+            sx={{ width: `20rem`, height: `1rem` }}
+          >
+            {colors.map(({ color, count, name }) => (
+              <Tooltip
+                title={
+                  <Typography>
+                    {name} {((count / total) * 100) | 0}%
+                  </Typography>
+                }
+                key={`${color?.join()}-${name}`}
+              >
+                <div
+                  style={{
+                    width: `${(count / total) * 100}%`,
+                    backgroundColor: `rgb(${color?.join()})`,
+                  }}
+                />
+              </Tooltip>
+            ))}
+          </Stack>
         </Stack>
-        <section>
+        <Stack gap={2} component="section">
           <Typography variant="h5" component="h3">
             Activity
           </Typography>
-          <MostRecent authorId={user.id} />
-        </section>
+          <Stack gap={1} component="ol">
+            {user.pastes.map(p => (
+              <Stack component="li" key={p.id}>
+                <NextLink href={`/pastes/${p.id}`}>
+                  {user.name} / {p.description}
+                </NextLink>
+                <Box>
+                  <pre>
+                    <code>
+                      {p.files[0].content.split(`\n`, 4).join(`\n`) + `\n...`}
+                    </code>
+                  </pre>
+                </Box>
+              </Stack>
+            ))}
+          </Stack>
+        </Stack>
       </Stack>
     </>
   )
